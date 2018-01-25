86a2a3ae20e133761f1825b7415b304118354c00
Christophe Lambercy <cly@cpnv.ch>
2018-01-25 23:00:20 +0100

    Lorsque que l'option "colonne" est choisie, les adresses sont copiées en format CSV et contiennent les noms et prénoms associés. Cela permet de créer très facilement un document excel ou sheet avec les noms / prénoms et adresses email des élèves ou des enseignants.

--- content_scripts/copy.js
+++ content_scripts/copy.js
@@ -16,24 +16,68 @@
     inputElt.remove();
   };
 
-  const copy = (selector, orientation) => {
+  const getEmails = (selector) => {
     const spanElt = document.querySelector(selector);
-    const emails = spanElt.getAttribute("data-clipboard-text").replace(/;;+/g, ';');
-    if (orientation === COLUMN) {
-      copyToClipboard(emails.replace(/\s*;\s*/g, "\n"));
-    } else {
-      copyToClipboard(emails);
-    }
+    return spanElt.getAttribute("data-clipboard-text").split(/\s*;\s*/);
   };
 
   const copyStudents = (orientation) => {
     console.log("copyStudents");
-    copy("#class_members span.clip-paste:nth-child(3)", orientation);
-  };
+    const emailEltSelector = "#class_members span.clip-paste:nth-child(3)";
+    copy(emailEltSelector, orientation, getStudentsList);
+  }
 
   const copyTeachers = (orientation) => {
     console.log("copyTeacher");
-    copy("h3 > span.clip-paste:nth-child(1)", orientation);
+    const emailEltSelector = "h3 > span.clip-paste:nth-child(1)";
+    copy(emailEltSelector, orientation, getTeachersList);
+  };
+
+  const copy = (emailSelector, orientation, listcb) => {
+    const emails = getEmails(emailSelector);
+    console.log(emails);
+    if (orientation === ROW) {
+      copyToClipboard(emails.join(';'));
+    } else {
+      let results = '';
+      const names = listcb();
+      console.log(JSON.stringify(names));
+      for (let i = 0, max = emails.length; i < max; i++) {
+        results += [names[i].join(';'), emails[i]].join(';') + "\n";
+      }
+      copyToClipboard(results);
+    }
+  };
+
+  const getStudentsList = () => {
+    var lastnames = [];
+    document.querySelectorAll("tr.record > td:nth-child(1) > a")
+      .forEach(e => lastnames.push(e.textContent));
+
+    var firstnames = [];
+    document.querySelectorAll("tr.record > td:nth-child(2)")
+      .forEach(e => firstnames.push(e.textContent));
+
+    var students = [];
+    for (let i = 0; i < lastnames.length; i++) {
+      students.push([lastnames[i], firstnames[i]]);
+    }
+    return students;
+  }
+
+  const getTeachersList = () => {
+    const teachers = [];
+    document
+      .querySelectorAll(".block > ul:nth-child(2) > li > a > span")
+      .forEach(elt => {
+        const res = elt.textContent.match(/(^[A-Z\s\-]+?)\s*([A-Z][a-z].*)/);
+        if (res !== null) {
+          teachers.push([res[1], res[2]]);
+        }
+      });
+
+    
+    return teachers;
   };
 
   browser.runtime.onMessage.addListener(message => {
