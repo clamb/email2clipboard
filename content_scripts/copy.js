f314d058b3aec7970fa62136127aec0fdf892155
Christophe Lambercy <christophe.lambercy@arcaciel.ch>
2018-12-06 19:03:16 +0100

    More flexibility when copying data

--- content_scripts/copy.js
+++ content_scripts/copy.js
@@ -1,11 +1,8 @@
-(function() {
+(function () {
   if (window.hasRun) {
     return;
   }
   window.hasRun = true;
-
-  const ROW    = 1;
-  const COLUMN = 2;
 
   const copyToClipboard = addresses => {
     const inputElt = document.createElement("textarea");
@@ -21,81 +18,138 @@
     return spanElt.getAttribute("data-clipboard-text").split(/\s*;\s*/);
   };
 
-  const copyStudents = (orientation) => {
-    console.log("copyStudents");
+  const copyStudents = (options) => {
+    // console.log("copyStudents");
     const emailEltSelector = "#class_members span.clip-paste:nth-child(3)";
-    copy(emailEltSelector, orientation, getStudentsList);
+    copy(emailEltSelector, getStudentsList, options);
   }
 
-  const copyTeachers = (orientation) => {
-    console.log("copyTeacher");
+  const copyTeachers = (options) => {
+    // console.log("copyTeacher");
     const emailEltSelector = "h3 > span.clip-paste:nth-child(1)";
-    copy(emailEltSelector, orientation, getTeachersList);
+    copy(emailEltSelector, getTeachersList, options);
   };
 
-  const copy = (emailSelector, orientation, listcb) => {
-    const emails = getEmails(emailSelector);
-    console.log(emails);
-    if (orientation === ROW) {
-      copyToClipboard(emails.join(';'));
-    } else {
-      let results = '';
-      const names = listcb();
-      console.log(JSON.stringify(names));
-      for (let i = 0, max = emails.length; i < max; i++) {
-        results += [names[i].join(';'), emails[i]].join(';') + "\n";
+  const getData = (names, emails, {
+    email,
+    fullname,
+    lastname,
+    firstname,
+    acronym
+  }) => {
+    // console.log(`names: ${JSON.stringify(names)}`);
+    // console.log(`emails: ${JSON.stringify(emails)}`);
+    const res = [];
+    for (let i = 0; i < names.length; i++) {
+      const each = [];
+
+      if (fullname) {
+        each.push(names[i][0] + ' ' + names[i][1]);
       }
-      copyToClipboard(results);
+
+      if (lastname) {
+        each.push(names[i][0]);
+      }
+
+      if (firstname) {
+        each.push(names[i][1]);
+      }
+
+      if (email) {
+        each.push(emails[i].toLowerCase());
+      }
+
+      if (acronym) {
+        each.push(names[i][2]);
+      }
+
+      res.push(each);
     }
+
+    // console.log(`res: ${JSON.stringify(res)}`);
+    return res;
+  };
+
+  const copy = (emailSelector, listcb, options) => {
+    // console.log(`options: ${JSON.stringify(options)}`);
+    const emails = getEmails(emailSelector);
+    const names = listcb();
+
+    const data = getData(names, emails, options)
+
+    let results = '';
+
+    data.forEach(rec => {
+      results += rec.join(options.fieldSeparator) + options.recordSeparator;
+    });
+
+    // console.log(`results:\n<${results}>`);
+    copyToClipboard(results);
   };
 
   const getStudentsList = () => {
-    var lastnames = [];
-    document.querySelectorAll("tr.record > td:nth-child(1) > a")
+    // querySelectorAll returns a NodeList that does not support map.
+    const lastnames = [];
+    document
+      .querySelectorAll("tr.record > td:nth-child(1) > a")
       .forEach(e => lastnames.push(e.textContent));
 
-    var firstnames = [];
-    document.querySelectorAll("tr.record > td:nth-child(2)")
+    const firstnames = [];
+    document
+      .querySelectorAll("tr.record > td:nth-child(2)")
       .forEach(e => firstnames.push(e.textContent));
 
-    var students = [];
+    const students = [];
     for (let i = 0; i < lastnames.length; i++) {
-      students.push([lastnames[i], firstnames[i]]);
+      const acronym = `${firstnames[i].toLowerCase()}_${lastnames[i].toLowerCase()}`;
+      students.push([lastnames[i], firstnames[i], acronym]);
     }
+
     return students;
   }
 
   const getTeachersList = () => {
-    const teachers = [];
+    const names = [];
     document
       .querySelectorAll(".block > ul:nth-child(2) > li > a > span")
       .forEach(elt => {
-        const res = elt.textContent.match(/(^[A-Z\s\-]+?)\s*([A-Z][a-z].*)/);
+        const res = elt.textContent.match(/(^[A-Z\s\-]+?)\s*([A-Z][a-z\u00C0-\u00FF].*)/u);
         if (res !== null) {
-          teachers.push([res[1], res[2]]);
+          names.push([res[1], res[2]]);
         }
       });
 
-    
+    const acronyms = [];
+    document
+      .querySelectorAll(".block > ul:nth-child(2) > li > a.resource")
+      .forEach(elt => {
+        acronyms.push(elt.getAttribute('href').split('/').pop());
+      });
+
+    const teachers = [];
+    for (let i = 0; i < names.length; i++) {
+      teachers.push([...names[i], acronyms[i]]);
+    }
     return teachers;
   };
 
-  browser.runtime.onMessage.addListener(message => {
-    switch (message.command) {
-      case 'copyTeachersRow':
-        copyTeachers(ROW);
-        break;
-      case 'copyTeachersColumn':
-        copyTeachers(COLUMN);
-        break;
-      case 'copyStudentsRow':
-        copyStudents(ROW);
-        break;
-      case 'copyStudentsColumn':
-        copyStudents(COLUMN);
-        break;
-      default:
-        break;
+  browser.runtime.onMessage.addListener(({
+    command,
+    options
+  }) => {
+    // console.log(`command: ${command}`);
+    // console.log(`options: ${JSON.stringify(options)}`);
+    if (options.email || options.fullname || options.lastname || options.acronym) {
+      switch (command) {
+        case 'copyTeachers':
+          copyTeachers(options);
+          break;
+        case 'copyStudents':
+          copyStudents(options);
+          break;
+        default:
+          break;
+      }
     }
   });
 })();
--- popup/copypage.js
+++ popup/copypage.js
@@ -1,30 +1,67 @@
 function listenForClicks() {
+  const cbEmail = document.getElementById("cbEmail");
+  const cbFullname = document.getElementById("cbFullname");
+  const cbLastname = document.getElementById("cbLastname");
+  const cbFirstname = document.getElementById("cbFirstname");
+  const cbAcronym = document.getElementById("cbAcronym");
+  const radioFieldSeparator = document.querySelectorAll("input[type=radio][name=separator]");
+  const radioRecordSeparator = document.querySelectorAll("input[type=radio][name=rseparator]");
 
+  const getSeparatorChar = v => {
+    switch (v) {
+      case 'cr':
+        return "\n";
 
-  const copyStudentsRow = tabs => {
+      case 'comma':
+        return ',';
+
+      case 'semicolon':
+        return ';'
+
+      case 'space':
+        return ' ';
+    }
+
+    return '';
+  };
+
+  const getSeparator = nodeList => {
+    for (const elt of nodeList) {
+      if (elt.checked) {
+        return getSeparatorChar(elt.value);
+      }
+    }
+
+    return '';
+  };
+
+  const getCopyDataOptions = () => {
+    let fieldSeparator = getSeparator(radioFieldSeparator);
+    let recordSeparator = getSeparator(radioRecordSeparator);
+
+    return {
+      email: cbEmail.checked,
+      fullname: cbFullname.checked,
+      lastname: cbLastname.checked,
+      firstname: cbFirstname.checked,
+      acronym: cbAcronym.checked,
+      fieldSeparator,
+      recordSeparator,
+    };
+  };
+
+  const copyStudents = tabs => {
     browser.tabs.sendMessage(tabs[0].id, {
-      command: "copyStudentsRow"
+      command: "copyStudents",
+      options: getCopyDataOptions()
     });
     notifyDone();
   };
 
-  const copyStudentsColumn = tabs => {
+  const copyTeachers = tabs => {
     browser.tabs.sendMessage(tabs[0].id, {
-      command: "copyStudentsColumn"
-    });
-    notifyDone();
-  };
-
-  const copyTeachersRow = tabs => {
-    browser.tabs.sendMessage(tabs[0].id, {
-      command: "copyTeachersRow"
-    });
-    notifyDone();
-  };
-
-  const copyTeachersColumn = tabs => {
-    browser.tabs.sendMessage(tabs[0].id, {
-      command: "copyTeachersColumn"
+      command: "copyTeachers",
+      options: getCopyDataOptions()
     });
     notifyDone();
   };
@@ -36,7 +73,7 @@ function listenForClicks() {
       message: "Les données sont copiées dans le presse-papier. CTRL-V ou CMD-V pour les coller."
     });
     window.close();
-  }
+  };
 
   const reportError = error => {
     browser.notifications.create({
@@ -47,79 +84,81 @@ function listenForClicks() {
     console.error(`Ooops, une erreur est survenue: ${error}`);
   };
 
-  document.querySelector("button#students-row").addEventListener("click", () => {
-    getCurrentTab()
-      .then(copyStudentsRow)
-      .catch(reportError);
-  });
-  document.querySelector("button#students-column").addEventListener("click", () => {
-    getCurrentTab()
-      .then(copyStudentsColumn)
-      .catch(reportError);
-  });
-  document.querySelector("button#teachers-row").addEventListener("click", () => {
-    getCurrentTab()
-      .then(copyTeachersRow)
-      .catch(reportError);
-  });
-  document.querySelector("button#teachers-column").addEventListener("click", () => {
-    getCurrentTab()
-      .then(copyTeachersColumn)
-      .catch(reportError);
-  });
+  document
+    .getElementById("students")
+    .addEventListener("click", () => {
+      getCurrentTab()
+        .then(copyStudents)
+        .catch(reportError);
+    });
 
-  const getCurrentTab = () => browser.tabs.query({
-    active: true,
-    currentWindow: true
-  });
+  document
+    .getElementById("teachers")
+    .addEventListener("click", () => {
+      getCurrentTab()
+        .then(copyTeachers)
+        .catch(reportError);
+    });
+
+  const getCurrentTab = () =>
+    browser.tabs.query({
+      active: true,
+      currentWindow: true
+    });
 }
 
-
 function reportExecuteScriptError(error) {
-  document.querySelector("#popup-content").classList.add("hidden");
-  document.querySelector("#error-content").classList.remove("hidden");
+  document.getElementById("popup-content").classList.add("hidden");
+  document.getElementById("error-content").classList.remove("hidden");
   console.error(`Failed to execute Email2Clipboard: ${error.message}`);
 }
 
-
 const manifest = browser.runtime.getManifest();
-document.querySelector("#version").textContent = `${manifest.name}, v${manifest.version}`;
+document.getElementById("version").textContent = `${manifest.name}, v${
+  manifest.version
+}`;
 
-const okelt = document.querySelector("#ok");
-const notokelt = document.querySelector("#notok");
+const okelt = document.getElementById("ok");
+const notokelt = document.getElementById("notok");
 
 chrome.tabs.query({
-  active: true,
-  lastFocusedWindow: true
-}, tabs => {
-  const url = tabs[0].url;
-  if (url.match(/https:\/\/intranet.cpnv.ch\/classes\/[^\/]+\/?$/) !== null) {
-    notokelt.classList.add('hidden');
-    okelt.classList.remove('hidden');
+    active: true,
+    lastFocusedWindow: true
+  },
+  tabs => {
+    const url = tabs[0].url;
+    if (url.match(/https:\/\/intranet.cpnv.ch\/classes\/[^\/]+\/?$/) !== null) {
+      notokelt.classList.add("hidden");
+      okelt.classList.remove("hidden");
 
-    browser.tabs
-      .executeScript({
-        file: "/content_scripts/copy.js"
-      })
-      .then(listenForClicks)
-      .catch(reportExecuteScriptError);
-  } else {
-    okelt.classList.add('hidden');
-    notokelt.classList.remove('hidden');
-    const matches = url.match(/https?:\/\/(intranet.cpnv.ch\/classes\/[^\/]+)\/?.*$/);
-    if (matches !== null) {
-      const classeUrl = "https://" + matches[1];
-      const urlelt = document.querySelector("#urlelt");
-      urlelt.insertAdjacentText('beforebegin', ' (');
-      urlelt.insertAdjacentText('afterend', ')');
-      urlelt.textContent = 'ici';
-      urlelt.addEventListener('click', event => {
-        event.preventDefault();
-        browser.tabs.update({ url: classeUrl });
-        setTimeout(() => {
-          window.close();
-        }, 500);
-      });
+      browser.tabs
+        .executeScript({
+          file: "/content_scripts/copy.js"
+        })
+        .then(listenForClicks)
+        .catch(reportExecuteScriptError);
+    } else {
+      okelt.classList.add("hidden");
+      notokelt.classList.remove("hidden");
+      const matches = url.match(
+        /https?:\/\/(intranet.cpnv.ch\/classes\/[^\/]+)\/?.*$/
+      );
+      if (matches !== null) {
+        const classeUrl = "https://" + matches[1];
+        const urlelt = document.querySelector("#urlelt");
+        urlelt.insertAdjacentText("beforebegin", " (");
+        urlelt.insertAdjacentText("afterend", ")");
+        urlelt.textContent = "ici";
+        urlelt.addEventListener("click", event => {
+          event.preventDefault();
+          browser.tabs.update({
+            url: classeUrl
+          });
+          setTimeout(() => {
+            window.close();
+          }, 500);
+        });
+      }
     }
   }
-});
+);
